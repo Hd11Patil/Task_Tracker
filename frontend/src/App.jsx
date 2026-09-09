e7f72a8fad201186499e278fb579084f153821c3
Hd11Patil <harshal.dp11@gmail.com>
2026-09-09 14:36:02 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Register from "./pages/Register";
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import CreateTask from './pages/CreateTask';
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import CreateTask from "./pages/CreateTask";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public Routes */}
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/create-task" element={<CreateTask />} />
+
+        {/* Protected Dashboard */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Protected Create Task */}
+        <Route
+          path="/create-task"
+          element={
+            <ProtectedRoute>
+              <CreateTask />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
