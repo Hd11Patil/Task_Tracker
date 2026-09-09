39432708f396f529f2fc422301a7d9a0b6572131
Hd11Patil <harshal.dp11@gmail.com>
2026-09-09 14:23:24 +0530

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://task-tracker-backend-i8ew.onrender.com',
 });
 
 export default api;
